a7aa78efa0154c90b832c9fd29e10cd83cae3b62
MD Shahnawaz <mdshahnawazm474@gmail.com>
2025-01-17 17:09:18 +0530

    Initial commit

---
+++ theme.config.jsx
@@ -0,0 +1,17 @@
+export default {
+  logo: <strong>Nextra Demo</strong>,
+  project: {
+    link: "https://github.com/sankthomas/nextra-yt-demo",
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: "Nextra Demo - %s",
+    };
+  },
+  footer: {
+    text: "Copyright MD Shahnawaz 2024",
+  },
+  chat: {
+    link: "https://discord.com",
+  },
+};
